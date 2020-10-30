2692192253516559f0bba89de355ebd1e99fdf2a
Venkat <Venkat.bca0@gmail.com>
2020-10-30 16:47:15 +0530

    Finding prime number using java script

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -1,10 +1,10 @@
-// Finding prime number
+// Finding Odd number
 
 var x=[30,15,2,18,35,45,83,97,14];
 var y=[];
 
 for(let i=0;i<x.length;i=i+1){
-if(x[i]%3===0){
+if(x[i]%2!==0){
   y.push(x[i]);
 }
 }
